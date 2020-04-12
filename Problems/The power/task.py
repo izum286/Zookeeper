1ae08c355b53bbfa5b4766f3e277a99b1b83aaa8
izum <i401623@gmail.com>
2020-04-12 20:24:34 +0300

    some tasks

---
+++ Problems/The power/task.py
@@ -0,0 +1,3 @@
+# put your python code here
+res = 31 ** 331 % 20
+print(res)
--- Zookeeper/task/zookeeper/zookeeper.py
+++ Zookeeper/task/zookeeper/zookeeper.py
@@ -1,4 +1,5 @@
-print("""Switching on camera from habitat with camels...
+camel = """
+Switching on camera from habitat with camels...
  ___.-''''-.
 /___  @    |
 ',,,,.     |         _.'''''''._
@@ -16,4 +17,113 @@ print("""Switching on camera from habitat with camels...
             ; ;    ! !    ! !     ; ;
            ;,,      !,!   !,!     ;,;
            /_I      L_I   L_I     /_I
-Yey, our little camel is sunbathing!""")
+Yey, our little camel is sunbathing!"""
+
+lion = """
+Switching on camera from habitat with lions...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is croaking!"""
+
+deer = """
+Switching on camera from habitat with deers...
+   /|       |\\
+`__\\\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \\
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!"""
+
+goose = """
+Switching on camera from habitat with lovely goose...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \\
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \\
+ <_  `     (  <`<            \              `-._\\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+This bird stares intently at you... (Maybe it's time to change the channel?)"""
+
+bat = """
+Switching on camera from habitat with bats...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\\
+                ~-. /  \_/  \ .-~
+                   V         V
+It looks like this bat is fine."""
+
+rabbit = """
+Switching on camera from habitat with rabbits...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\//\/      .- \\
+ Y        /    Y 
+ l       I     !
+ ]\      _\    /"\\
+(" ~----( ~   Y.  )
+It seems there will be more rabbits soon!"""
+
+# write your code here
+my_list = list()
+my_list = [camel, lion, deer, goose, bat, rabbit]
+habitat = int(input("Which habitat # do you need? > "))
+print(my_list[habitat])
+print("---")
+print("The end of the program. To check another habitat restart the watcher please.")
+
